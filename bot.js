14e8599883ed4993962c16c71f292c76ecf97127
Grace Kim <gracekim@ibm.com@Graces-MacBook-Pro.local>
2020-07-06 16:17:28 -0700

    bot working, website up

--- bot.js
+++ bot.js
@@ -1,6 +1,10 @@
 // username must be named a mod in the channel -> /mod <username> in channel's chat
 
 const tmi = require('tmi.js');
+const csv = require('csv-parser');
+const fs = require('fs');
+var shuffle = require('shuffle-array');
+const http = require('http');
 
 const options = {
   options: {
@@ -14,15 +18,20 @@ const options = {
 };
 
 const channel = 'dujsnd';
-var question = 'do u like mgg?';
-var answer = 'yes';
+const file = 'trivia.csv';
+var curr = 0;
+var questionSet = [];
+var question = '';
+var answer = '';
 
 const client = new tmi.client(options);
 
 client.connect();
+loadQuestions(file);
 
 client.on('connected', onConnectedHandler);
 client.on('chat', onChatHandler);
+createWebsite();
 
 function onConnectedHandler(address, port) {
   client.action(channel, 'bot has connected');
@@ -31,14 +40,46 @@ function onConnectedHandler(address, port) {
 
 function onChatHandler(channel, user, message, self) {
   if (self) { return };
-
-  if (message === answer) {
+  if (message.toLowerCase() === answer.toLowerCase()) {
     var name = user["display-name"] || user["username"];
     client.action(channel, `${name} guessed the correct answer: ${answer}`);
     askQuestion();
   }
 };
 
+function loadQuestions(file) {
+	fs.createReadStream(file)
+  .pipe(csv())
+  .on('data', (row) => {
+		console.log(row);
+		questionSet.push(row) //appends row to questionSet array
+  })
+  .on('end', () => {
+		console.log('CSV file successfully processed');
+  });
+  shuffle(questionSet);
+}
+
 function askQuestion() {
-  client.action(channel, question);
+  if (curr === questionSet.length) {
+    client.action(channel, 'Those are all questions, thanks for playing!');
+    return;
+  }
+  question = questionSet[curr].question;
+  answer = questionSet[curr].answer;
+  curr = curr + 1;
+  client.action(channel, `Question #${curr} of ${questionSet.length}: ${question}`);
+}
+
+
+
+
+
+
+
+function createWebsite() {
+  http.createServer(function (req, res){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+  }).listen(8080);
 }
