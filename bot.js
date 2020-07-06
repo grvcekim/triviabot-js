88f0cb3d1a5ea968515234ba00ff8cb0edff111c
Grace Kim <gracekim@ibm.com@Graces-MacBook-Pro.local>
2020-07-06 03:36:24 -0700

    global single q/a

--- bot.js
+++ bot.js
@@ -1,102 +1,44 @@
+// username must be named a mod in the channel -> /mod <username> in channel's chat
+
 const tmi = require('tmi.js');
 
-// Define configuration options
-const opts = {
-  identity: {
-    username: "triviabot",
-    password: "oauth:085klbdmtxwcmnkrzeoliiylrng33i"
+const options = {
+  options: {
+    debug: true,
   },
-  channels: [
-    "mhrosen"
-  ]
+  identity: {
+    username: 'grvcekim',
+    password: 'oauth:am6u2pot7mgiiuhzuhno34nl6uhrby',
+  },
+  channels: ['dujsnd'],
 };
 
-// Create a client with our options
-const client = new tmi.client(opts);
+const channel = 'dujsnd';
+var question = 'do u like mgg?';
+var answer = 'yes';
 
-// Register our event handlers (defined below)
-client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
+const client = new tmi.client(options);
 
-// Connect to Twitch:
 client.connect();
 
-// Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
+client.on('connected', onConnectedHandler);
+client.on('chat', onChatHandler);
 
-  // Remove whitespace from chat message
-  const commandName = msg.trim();
+function onConnectedHandler(address, port) {
+  client.action(channel, 'bot has connected');
+  askQuestion();
+};
 
-  // If the command is known, let's execute it
-  if (commandName === '!dice') {
-    const num = rollDice();
-    client.say(target, `You rolled a ${num}`);
-    console.log(`* Executed ${commandName} command`);
-  } else {
-    console.log(`* Unknown command ${commandName}`);
+function onChatHandler(channel, user, message, self) {
+  if (self) { return };
+
+  if (message === answer) {
+    var name = user["display-name"] || user["username"];
+    client.action(channel, `${name} guessed the correct answer: ${answer}`);
+    askQuestion();
   }
+};
+
+function askQuestion() {
+  client.action(channel, question);
 }
-// Function called when the "dice" command is issued
-function rollDice () {
-  const sides = 6;
-  return Math.floor(Math.random() * sides) + 1;
-}
-// Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
-  console.log(`* Connected to ${addr}:${port}`);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// var question = "where do u intern y'allsllszzz"
-// var answer = "ibm"
-
-// // Called every time a message comes in
-// function onMessageHandler (target, context, msg, self) {
-//   if (self) { return; } // Ignore messages from the bot
-
-//   // Remove whitespace from chat message
-//   const commandName = msg.trim();
-
-//   if (commandName === answer) {
-//     client.say(target, `Correct answer!`);
-//     console.log(`* Correct guess: ${answer}`);
-//   } else {
-//     client.say(target, `Try again!`);
-//     console.log(`* Incorrect guess: ${commandName}`);
-//   }
-//   // If the command is known, let's execute it
-//   // if (commandName === '!dice') {
-//   //   const num = rollDice();
-//   //   client.say(target, `You rolled a ${num}`);
-//   //   console.log(`* Executed ${commandName} command`);
-//   // } else {
-//   //   console.log(`* Unknown command ${commandName}`);
-//   // }
-// }
-// // Function called when the "dice" command is issued
-// // function rollDice () {
-// //   const sides = 6;
-// //   return Math.floor(Math.random() * sides) + 1;
-// // }
-
-// // Called every time the bot connects to Twitch chat
-// function onConnectedHandler (target, addr, port) {
-//   console.log(`* Connected to ${addr}:${port}`);
-//   // client.say(target, question);
-//   // console.log(`* Asked question: ${question}`);
-// }
